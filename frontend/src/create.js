ec9ccab41daf8aec3a6d29900f6604ed3af99140
Henry Crossley <crossh2001@yahoo.com>
2022-11-26 00:00:54 -0800

    working on create js

--- frontend/src/create.js
+++ frontend/src/create.js
@@ -9,3 +9,21 @@ export default function Create() {
       level: "",
     });
 }
+
+/*await createListing(client,
+            {
+                name: "cool little house",
+                summary: "A charming loft in Paris",
+                bedrooms: 2,
+                bathrooms: 1
+            }
+        );*/
+
+
+        /*
+        async function createListing(client, newListing){
+        const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+        console.log(`New listing created with the following id: ${result.insertedId}`);
+    
+        }
+        */ 
