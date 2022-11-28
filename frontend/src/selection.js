f4e1fe07af712c7fd21d7a6a687abf03218b452a
Henry Crossley <crossh2001@yahoo.com>
2022-11-28 02:22:02 -0800

    added react-router-dom

--- frontend/src/selection.js
+++ frontend/src/selection.js
@@ -1,11 +1,13 @@
 import REACT from "react";
 import {useState} from "react";
 import CREATE from "./create";
+//import { useParams, useNavigate } from "react-router";
+//import { Link } from "react-router-dom";
 
 export default function Selection(){
 
     function createTest(){
-        alert("creation page works");
+        <></>
     }
 
     return(<>
@@ -13,6 +15,7 @@ export default function Selection(){
             Please Press a Button
         </h1>
         <button id="create" onClick={createTest}> Create</button>
+        <CREATE/>
     </>);
 }
 
